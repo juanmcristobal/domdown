cd17f5b7ca4972bad50894023bcb44df9492333f
Juan Manuel Cristóbal Moreno <juanmcristobal@gmail.com>
2026-05-22 09:46:55 +0200

    delete old package

--- domdown/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from domdown.domdown import Domdown
-from domdown.types import DomdownOptions, DomdownResponse
-
-__all__ = ["Domdown", "DomdownOptions", "DomdownResponse"]
